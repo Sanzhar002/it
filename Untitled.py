4109ba9fb83a2d892acd68bf5db2505a6cfb629f
Sanzhar002 <78396985+Sanzhar002@users.noreply.github.com>
2021-02-16 10:35:08 +0600

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+if 1:
+    print("hello 1")
+    
+
+
+# In[2]:
+
+
+a=3
+if a==3:
+    print("здесь а=",a)
+
+
+# In[3]:
+
+
+if a>1:
+    print("hello 3")
+
+
+# In[ ]:
+
+
+
+
